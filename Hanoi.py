9a2bf83fbd3b05d373d2cba1f69a88cb02590811
Максим Зубков <survivingdreamer@mail.ru>
2022-11-15 20:24:38 +0300

    Solving the Hanoi tower

--- Hanoi.py
+++ Hanoi.py
@@ -1,12 +1,12 @@
 import sys
-sys.setrecursionlimit (1000000)
+sys.setrecursionlimit(1000000)
 
-def Hanoi(n , A, B, C):
+def Hanoi(n, A, B, C):
     if n == 1:
         print("Переместим диск 1 с башни", A, "на башню", C)
-        return 
-    Hanoi(n-1, A, C, B)
+        return
+    Hanoi(n - 1, A, C, B)
     print("Переместим диск", n, "с башни", A, "на башню", C)
-    Hanoi(n-1, B, A, C)
-          
+    Hanoi(n - 1, B, A, C)
+
 Hanoi(5, 'A', 'B', 'C')
